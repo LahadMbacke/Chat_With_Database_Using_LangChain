05cfe2b4e12ba4a6efa565f8600051d1016c69dc
Lahad Mbacke <lahadmbacke00@gmail.com>
2024-07-12 19:13:26 +0200

    load env variables

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ import psycopg2
 
 
 load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+host = os.getenv("host")
+port = os.getenv("port")
+dbname = os.getenv("dbname")
+username = os.getenv("username")
+password = os.getenv("password")
 
 prompt = """En se basant sur ce schema , ecrit un requte sql pour repondre la question d'un utilisatuer {schema}.
 Question : {question}"""
 
 template = ChatPromptTemplate.from_template(prompt)
-print(template.format(schema="Student",question="Quels sont les etudiants mineurs"))
+# print(template.format(schema="Student",question="Quels sont les etudiants mineurs"))
 
-uri_pg = f"postgresql+psycopg2//{username}:{password}@{host}:{port}/{dbname}"
+uri_pg = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{dbname}"
+
 
 db = SQLDatabase.from_uri(uri_pg)
